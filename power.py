9708944505885ef39c8eb3f3de99b1f740dfe9d3
Yatri Trivedi <jivanyatra@gmail.com>
2014-08-18 21:17:50 -0400

    fixed function parameter, list appending, and return values

--- power.py
+++ power.py
@@ -1,12 +1,13 @@
 def populate_resistances(list):
-    reses = []
+    list = []
     list_1to3 = xrange(10, 30, 2)
     list_3to10 = xrange(35, 100, 5)
     list_10to30 = xrange(10, 30)
     for each in list_1to3:
-        reses.append(list_1to3/100.)
+        list.append(list_1to3/100.)
     for each in list_3to10:
-        reses.append(list_3to10/100.)
+        list.append(list_3to10/100.)
     for each in list_10to30:
-        reses.append(list_10to30/10.)
-    return reses
+        list.append(list_10to30/10.)
+    return list
+
