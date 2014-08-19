d33bce7b08faeeb85d78b2e4590afe845b0a182b
Yatri Trivedi <jivanyatra@gmail.com>
2014-08-18 21:19:51 -0400

    fixed list building. dumb mistake.

--- power.py
+++ power.py
@@ -4,10 +4,10 @@ def populate_resistances(list):
     list_3to10 = xrange(35, 100, 5)
     list_10to30 = xrange(10, 30)
     for each in list_1to3:
-        list.append(list_1to3/100.)
+        list.append(each/100.)
     for each in list_3to10:
-        list.append(list_3to10/100.)
+        list.append(each/100.)
     for each in list_10to30:
-        list.append(list_10to30/10.)
+        list.append(each/10.)
     return list
 
